be322bbe52943cea33bdb264c14db6629ade0491
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2016-06-04 02:10:52 -0300

    Implemented midi loading feature

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,19 @@ module Main where
 
 import           Control.Concurrent
 import           Control.Monad
+import qualified Data.ByteString.Lazy as BL
 import           Data.Maybe
 import           Data.IORef
 import           Data.Time.Clock
 import           FRP.Yampa
 import           Sound.MidiPlayer
-import qualified Sound.MIDI.File.Event as MidiFile.Event
 import qualified Sound.MIDI.File.Load as MidiFile.Load
 
 import qualified Sound.MIDI.File as MidiFile
-import qualified Sound.MIDI.Message.Channel.Voice as VoiceMsg
-import qualified Sound.MIDI.Message.Channel       as ChannelMsg
-import Data.EventList.Relative.MixedBody ((/.), (./), )
-import qualified Data.EventList.Relative.TimeBody as EventList
+import qualified Sound.MIDI.Parser.Report as MidiParser
 
 import qualified Sound.PortMidi as PM
+import           System.Directory
 import           System.Exit
 
 initialize :: IO UIEvent
@@ -45,22 +43,37 @@ outputActuate streamSem stream _ res =
         putStrLn ("Output: " ++ str)
         pure False
 
+askMidiFile :: IO MidiFile.T
+askMidiFile = askMidiFile' Nothing
+  where
+    askMidiFile' = fromMaybe $ do
+      putStrLn "Enter midifile path:"
+      path <- getLine
+      putStrLn $ "Opening " ++ path
+      exists <- doesFileExist path
+      if exists then do
+        file <- BL.readFile path
+        let mMidifile = MidiFile.Load.maybeFromByteString file
+        case MidiParser.result mMidifile of
+          Left err -> do
+            putStrLn $ "Error " ++ err
+            putStrLn "Try again..."
+            askMidiFile' Nothing
+          Right midifile -> do
+            putStrLn $ "Loading " ++ path
+            pure midifile
+      else do
+        putStrLn $ "File " ++ path ++ " does not exist"
+        putStrLn "Try again..."
+        askMidiFile' Nothing
+
 askCmd :: MVar UICmd -> IO ()
 askCmd cmdVar = forever $ do
   putStrLn "\nEnter cmd:"
   cmd <- getLine
   uiCmd <- case cmd of
     "load" -> do
-      putStrLn "Enter midifile path:"
-      path <- getLine
-      midifile <- case path of
-        "default" ->
-          pure midiExample
-        _ -> do
-          putStrLn "Opening /tmp/Flourish.mid"
-          file <- MidiFile.Load.fromFile "/tmp/Flourish.mid"
-          putStrLn "Loading /tmp/Flourish.mid"
-          pure file
+      midifile <- askMidiFile
       pure $ LoadMidi midifile
     "playPause" -> do
       putStrLn "Pressed Play/Pause button"
@@ -71,19 +84,6 @@ askCmd cmdVar = forever $ do
     _ ->
       pure NoCmd
   putMVar cmdVar uiCmd
-
-midiExample :: MidiFile.T
-midiExample =
-   let chan = ChannelMsg.toChannel 3
-       vel  = VoiceMsg.toVelocity 64
-   in  MidiFile.Cons MidiFile.Parallel (MidiFile.Ticks 10)
-          [0 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOn (VoiceMsg.toPitch 20) vel))) ./
-           4 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOn (VoiceMsg.toPitch 24) vel))) ./
-           4 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOn (VoiceMsg.toPitch 27) vel))) ./
-           7 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOff (VoiceMsg.toPitch 20) vel))) ./
-           4 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOff (VoiceMsg.toPitch 24) vel))) ./
-           4 /. MidiFile.Event.MIDIEvent (ChannelMsg.Cons chan (ChannelMsg.Voice (VoiceMsg.NoteOff (VoiceMsg.toPitch 27) vel))) ./
-           EventList.empty]
 
 main :: IO ()
 main = do
