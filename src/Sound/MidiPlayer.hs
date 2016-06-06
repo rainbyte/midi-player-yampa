1f43940a931bf1bc103b9d7c3168ebe4baea0250
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2016-06-05 22:37:22 -0300

    R&C: factored out transition function

--- src/Sound/MidiPlayer.hs
+++ src/Sound/MidiPlayer.hs
@@ -8,13 +8,16 @@ module Sound.MidiPlayer
     , midiPlayer
     ) where
 
-import           Data.Maybe
 import           FRP.Yampa
 import qualified Sound.MIDI.File as MidiFile
 import qualified Sound.MIDI.PortMidi as MidiPM
 import qualified Sound.PortMidi as PM
 
 type MidiCmd = Either String PM.PMMsg
+data PlayerState = Initial
+                 | Playing MidiFile.T
+                 | Stopped MidiFile.T
+                 | Paused MidiFile.T
 data UICmd = LoadMidi !MidiFile.T | PlayPause | Stop | NoCmd
   deriving Show
 
@@ -25,49 +28,42 @@ midiPlayer :: SF UIEvent MidiEvent
 midiPlayer = initial
 
 initial :: SF UIEvent MidiEvent
-initial = switch (arr trigger) id
+initial = switch (arr trigger) nextSF
   where
-    trigger :: UIEvent -> (MidiEvent, Event (SF UIEvent MidiEvent))
-    trigger uiEv =
-      case fromEvent uiEv of
-        LoadMidi file -> (NoEvent, Event $ stopped file)
-        _ -> (NoEvent, NoEvent)
+    trigger uiEv = (NoEvent, transition Initial (fromEvent uiEv))
+    nextSF f = f never
 
-playing :: MidiFile.T -> Maybe (SF UIEvent MidiEvent) -> SF UIEvent MidiEvent
-playing file mCont =
-  dkSwitch (fromMaybe sf mCont) (arr trigger >>> notYet) nextSF
+playing :: MidiFile.T -> SF UIEvent MidiEvent -> SF UIEvent MidiEvent
+playing file sf = dkSwitch sf (arr trigger >>> notYet) nextSF
   where
-    sf = proc uiEv -> do
-      midiEv <- midiStep file -< uiEv
-      returnA -< midiEv
-    trigger (uiEv, _) =
-      case fromEvent uiEv of
-        LoadMidi newFile -> Event $ const (stopped newFile)
-        PlayPause -> Event $ paused file
-        Stop -> Event $ const (stopped file)
-        _ -> NoEvent
-    nextSF cont f = f cont
+    trigger (uiEv, _) = transition (Playing file) (fromEvent uiEv)
+    nextSF oldSF f = f oldSF
 
 midiStep :: MidiFile.T -> SF UIEvent MidiEvent
 midiStep !midiFile =
   let pairs = MidiPM.fromMidiFileRelative midiFile
   in afterEachCat . fmap (first fromRational) $ pairs
 
-stopped :: MidiFile.T -> SF UIEvent MidiEvent
-stopped file = switch (arr trigger >>> (notYet *** notYet)) id
+stopped :: MidiFile.T -> SF UIEvent MidiEvent -> SF UIEvent MidiEvent
+stopped file _ = switch (arr trigger >>> (notYet *** notYet)) nextSF
   where
-    trigger uiEv =
-      case fromEvent uiEv of
-        LoadMidi newFile -> (NoEvent, Event $ stopped newFile)
-        PlayPause -> (NoEvent, Event $ playing file Nothing)
-        _ -> (NoEvent, NoEvent)
+    trigger uiEv = (NoEvent, transition (Stopped file) (fromEvent uiEv))
+    nextSF f = f (midiStep file)
 
 paused :: MidiFile.T -> SF UIEvent MidiEvent -> SF UIEvent MidiEvent
-paused file cont = dkSwitch never (arr trigger >>> notYet) (flip const)
+paused file cont = dkSwitch never (arr trigger >>> notYet) nextSF
   where
-    trigger (uiEv, _) =
-      case fromEvent uiEv of
-        LoadMidi newFile -> Event $ stopped newFile
-        PlayPause -> Event $ playing file (Just cont)
-        Stop -> Event $ stopped file
-        _ -> NoEvent
+    trigger (uiEv, _) = transition (Paused file) (fromEvent uiEv)
+    nextSF _ f = f cont
+
+transition :: PlayerState
+           -> UICmd
+           -> Event (SF UIEvent MidiEvent -> SF UIEvent MidiEvent)
+transition state uiCmd = case (state, uiCmd) of
+  (_           , LoadMidi file) -> Event $ stopped file
+  (Playing file, Stop)          -> Event $ stopped file
+  (Paused file , Stop)          -> Event $ stopped file
+  (Playing file, PlayPause)     -> Event $ paused file
+  (Stopped file, PlayPause)     -> Event $ playing file
+  (Paused file , PlayPause)     -> Event $ playing file
+  _                             -> NoEvent
